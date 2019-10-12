31086b245e7e7e41d1ac8515cfdff8666807aeee
lukfor <lukas.forer@i-med.ac.at>
2019-10-12 14:15:26 +0200

    Prepare release 0.1.2

--- src/main/java/genepi/imputationbot/App.java
+++ src/main/java/genepi/imputationbot/App.java
@@ -21,7 +21,7 @@ import genepi.imputationbot.commands.ShowVersion;
 
 public class App extends Toolbox {
 
-	public static final String VERSION = "0.1.1";
+	public static final String VERSION = "0.1.2";
 
 	public App(String command, String[] args) {
 		super(command, args);
