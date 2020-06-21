e59032cd7cebcba25ae3076448812b4ec1f0cc54
lukfor <lukas.forer@i-med.ac.at>
2020-06-21 13:10:19 +0200

    Update test-container to imputationserver-1.4.1

--- src/test/java/genepi/imputationbutler/ImputationServer.java
+++ src/test/java/genepi/imputationbutler/ImputationServer.java
@@ -27,7 +27,7 @@ public class ImputationServer {
 
 	public static final String IMAGE = "genepi/imputationserver";
 
-	public static final String VERSION = "v1.2.7";
+	public static final String VERSION = "v1.4.1";
 
 	public static final String LOCAL_DIRECTORY = "imputationserver_" + VERSION;
 
