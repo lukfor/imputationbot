ef2c655104ddb4d9de98d8e68c9c94ea9f0151fa
Lukas Forer <lukas.forer@i-med.ac.at>
2020-09-01 13:16:06 +0200

    Fix token issues in instances command

--- src/main/java/genepi/imputationbot/commands/ListInstances.java
+++ src/main/java/genepi/imputationbot/commands/ListInstances.java
@@ -9,12 +9,13 @@ import genepi.imputationbot.client.CloudgeneApiToken;
 import genepi.imputationbot.client.CloudgeneClient;
 import genepi.imputationbot.client.CloudgeneInstance;
 import genepi.imputationbot.client.CloudgeneUser;
+import genepi.imputationbot.util.AnsiColors;
 import genepi.imputationbot.util.FlipTable;
 
 public class ListInstances extends BaseCommand {
 
 	private String[][] data;
-	
+
 	public ListInstances(String... args) {
 		super(args);
 	}
@@ -34,8 +35,8 @@ public class ListInstances extends BaseCommand {
 
 		List<CloudgeneInstance> instances = new Vector<CloudgeneInstance>(getInstanceList().getAll());
 
-		CloudgeneClient client = getClient();
-		
+		CloudgeneClient client = getClient(false);
+
 		String[] header = new String[6];
 		header[0] = "ID";
 		header[1] = "Name";
@@ -43,33 +44,41 @@ public class ListInstances extends BaseCommand {
 		header[3] = "Username";
 		header[4] = "Version";
 		header[5] = "Token expires on";
-		
-		data = new String[instances.size()][header.length];
-		
-		for (int i = 0; i < instances.size(); i++) {
-			
-			CloudgeneInstance instance = instances.get(i);
-			
-			JSONObject server = client.getServerDetails(instance);
-			JSONObject app = client.getDefaultApp(instance);
-			CloudgeneUser user = client.getAuthUser(instance);
-			CloudgeneApiToken token = client.verifyToken(instance, instance.getToken());
 
-			data[i][0] = (i+1)+"";
-			data[i][1] = server.getString("name");
-			data[i][2] = instance.getHostname();
-			data[i][3] = user.getUsername();
-			data[i][4] = app.getString("version");
-			data[i][5] =token.getExpire().toString();
+		data = new String[instances.size()][header.length];
+
+		for (int i = 0; i < instances.size(); i++) {
+
+			CloudgeneInstance instance = instances.get(i);
+
+			data[i][0] = (i + 1) + "";
+
+			CloudgeneApiToken token = client.verifyToken(instance, instance.getToken());
+			if (token.isValid() && !token.isExpired()) {
+				JSONObject server = client.getServerDetails(instance);
+				JSONObject app = client.getDefaultApp(instance);
+				CloudgeneUser user = client.getAuthUser(instance);
+				data[i][1] = server.getString("name");
+				data[i][2] = instance.getHostname();
+				data[i][3] = user.getUsername();
+				data[i][4] = app.getString("version");
+				data[i][5] = AnsiColors.makeGreen(token.getExpire().toString());
+			} else {
+				data[i][1] = "-";
+				data[i][2] = instance.getHostname();
+				data[i][3] = token.getUsername();
+				data[i][4] = "-";
+				data[i][5] = AnsiColors.makeRed(token.getExpire().toString());
+			}
 		}
 
-		String table = FlipTable.of(header, data) + "\n";		
+		String table = FlipTable.of(header, data) + "\n";
 		println(table);
-		
+
 		return 0;
 
 	}
-	
+
 	public String[][] getData() {
 		return data;
 	}
