a33debabeaf1410d5fef32e3b76360f2084bc6f5
Lukas Forer <lukas.forer@i-med.ac.at>
2020-12-11 19:06:01 +0100

    Prepare release 0.9.2

--- install/github-downloader.sh
+++ install/github-downloader.sh
@@ -3,7 +3,7 @@
 set -e
 
 NAME="Imputation Bot"
-VERSION="v0.9.1"
+VERSION="v0.9.2"
 GITHUB_USER="lukfor"
 GITHUB_REPO="imputationbot"
 EXECUTABLE="imputationbot"
--- src/main/java/genepi/imputationbot/App.java
+++ src/main/java/genepi/imputationbot/App.java
@@ -24,7 +24,7 @@ import genepi.imputationbot.util.OperatingSystem;
 
 public class App extends Toolbox {
 
-	public static final String VERSION = "0.9.1";
+	public static final String VERSION = "0.9.2";
 
 	public App(String command, String[] args) {
 		super(command, args);
