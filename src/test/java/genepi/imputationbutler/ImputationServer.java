3fd001bd085b922affbb5433ce29ef0e2478e276
lukas <lukas@192.168.0.157>
2020-12-14 14:19:32 +0100

    Update testcases to imputationserver 1.5.7 and cloudgene 2.4.1

--- src/test/java/genepi/imputationbutler/ImputationServer.java
+++ src/test/java/genepi/imputationbutler/ImputationServer.java
@@ -28,7 +28,7 @@ public class ImputationServer {
 
 	public static final String IMAGE = "genepi/imputationserver";
 
-	public static final String VERSION = "v1.4.1";
+	public static final String VERSION = "v1.5.7";
 
 	public static final String LOCAL_DIRECTORY = "imputationserver_" + VERSION;
 
