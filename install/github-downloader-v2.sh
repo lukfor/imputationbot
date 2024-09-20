82449f495944336efc98bba60aedd524dd872975
Lukas Forer <lukas.forer@i-med.ac.at>
2024-09-20 15:03:25 +0200

    Create github-downloader-v2.sh

---
+++ install/github-downloader-v2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+NAME="Imputation Bot"
+VERSION="v2.0.0"
+GITHUB_USER="lukfor"
+GITHUB_REPO="imputationbot"
+EXECUTABLE="imputationbot"
+INSTALLER_SCRIPT="imputationbot-installer.sh"
+
+INSTALLER_URL=https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/download/${VERSION}/${INSTALLER_SCRIPT}
+
+
+echo "Installing ${NAME} ${VERSION}..."
+
+echo "Downloading ${NAME} from ${INSTALLER_URL}..."
+curl -fL ${INSTALLER_URL} -o ${INSTALLER_SCRIPT}
+
+# execute installer
+chmod +x ./${INSTALLER_SCRIPT}
+./${INSTALLER_SCRIPT}
+
+# change mod for executables
+chmod +x ./${EXECUTABLE}
+
+# remove installer
+rm ./${INSTALLER_SCRIPT}
+
+echo ""
+GREEN='\033[0;32m'
+NC='\033[0m'
+echo -e "${GREEN}${NAME} ${VERSION} installation completed. Have fun!${NC}"
+echo ""
