919b3b5176979bf35bdc7cbdb5ce225e5ec880ea
Lukas Forer <lukas.forer@i-med.ac.at>
2024-09-20 16:59:33 +0200

    Prepare release 2.0.0

--- src/main/java/genepi/imputationbot/App.java
+++ src/main/java/genepi/imputationbot/App.java
@@ -24,7 +24,7 @@ import genepi.imputationbot.util.OperatingSystem;
 
 public class App extends Toolbox {
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "2.0.0";
 
 	public App(String command, String[] args) {
 		super(command, args);
